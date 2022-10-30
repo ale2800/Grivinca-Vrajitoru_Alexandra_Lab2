663b51fb2aef7563e1724f9020b4e7122ac417b7
Lenovo <Lenovo@DESKTOP-8Q0H6AS>
2022-10-30 10:10:27 +0200

    Add project files.

---
+++ Grivinca-Vrajitoru_Alexandra_Lab2/Data/Grivinca_Vrajitoru_Alexandra_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Grivinca_Vrajitoru_Alexandra_Lab2.Models;
+
+namespace Grivinca_Vrajitoru_Alexandra_Lab2.Data
+{
+    public class Grivinca_Vrajitoru_Alexandra_Lab2Context : DbContext
+    {
+        public Grivinca_Vrajitoru_Alexandra_Lab2Context (DbContextOptions<Grivinca_Vrajitoru_Alexandra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grivinca_Vrajitoru_Alexandra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Grivinca_Vrajitoru_Alexandra_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Grivinca_Vrajitoru_Alexandra_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Grivinca-Vrajitoru_Alexandra_Lab2/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Grivinca_Vrajitoru_Alexandra_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
